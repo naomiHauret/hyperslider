99db78fe0dc6a26abfb970fc2774eb419bbb7d0d
guillaume <guillaume-duclos@hotmail.fr>
2018-03-01 18:48:52 +0100

    Ajout du fichier configs pour les données de connections à firebase

---
+++ src/configs/index.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyCy1OpspTqgf6H_5yN-61hDm6IWmWT0Ruc",
+    authDomain: "hyperslider-48c33.firebaseapp.com",
+    databaseURL: "https://hyperslider-48c33.firebaseio.com",
+    projectId: "hyperslider-48c33",
+    storageBucket: "hyperslider-48c33.appspot.com",
+    messagingSenderId: "216189201369"
+}
